485965eed447a9b688d40e52b512dd1bb363c962
IBM <IBM@DEVOPS_12>
2018-07-30 23:37:55 +0530

    pusheddd

---
+++ test.one.two/Feature/mytest.feature
@@ -0,0 +1,6 @@
+Feature: To Test shopping service
+@login
+Scenario: To validate login application
+Given Login application should be available
+When validate login application
+Then login should be successful
---
+++ test.one.two/src/test/java/StepDefinition/StepDefinition.java
@@ -0,0 +1,40 @@
+package StepDefinition;
+
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class StepDefinition {
+	WebDriver ffdriver1;
+	String title;
+	@Given("^Login application should be available$")
+	public void login_application_should_be_available() {
+	    // Write code here that turns the phrase above into concrete actions
+		System.setProperty("webdriver.gecko.driver","E:\\gecko\\geckodriver.exe");
+		  ffdriver1=new FirefoxDriver();
+			ffdriver1.get("http://ec2-18-191-84-26.us-east-2.compute.amazonaws.com:8090/TestWebapp");
+			ffdriver1.manage().window().maximize();
+	}
+
+	@When("^validate login application$")
+	public void validate_login_application()  {
+	    // Write code here that turns the phrase above into concrete actions
+		title=ffdriver1.getTitle();
+		System.out.println(title);
+		
+		Assert.assertNotSame("The Apache foundation ",title );
+	    	}
+
+	@Then("^login should be successful$")
+	public void login_should_be_successful()  {
+	    // Write code here that turns the phrase above into concrete actions
+	   
+	}
+
+	
+}
